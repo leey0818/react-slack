07bcdafd6736003a47b8201885e4e4279f0d7ed4
leey0818 <leey0818@gmail.com>
2021-09-27 17:27:26 +0900

    chore(webpack): change webpack-dev-server port

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,83 @@
 const path = require('path');
 const webpack = require('webpack');
+const portfinder = require('portfinder');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
 const dotenv = require('dotenv').config();
 if (dotenv.error) {
   throw dotenv.error;
 }
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
-const config = {
-  mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-  entry: './src/main',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js',
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-    alias: {
-      // '@': path.resolve(__dirname, 'src'),
-      '@api': path.resolve(__dirname, 'src/api'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@layouts': path.resolve(__dirname, 'src/layouts'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
+const getPort = port => portfinder.getPortPromise({ port });
+const getConfig = async () => {
+  const config = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: './src/main',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'app.js',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts|tsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+      alias: {
+        // '@': path.resolve(__dirname, 'src'),
+        '@api': path.resolve(__dirname, 'src/api'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+        '@layouts': path.resolve(__dirname, 'src/layouts'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
       },
-      {
-        test: /\.(png|jpe?g|gif|woff|woff2|ttf|svg|ico)$/i,
-        loader: 'file-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts|tsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.(png|jpe?g|gif|woff|woff2|ttf|svg|ico)$/i,
+          loader: 'file-loader',
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: 'public/index.html' }),
+      new ForkTsCheckerWebpackPlugin({ async: false }),
+      new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'public/index.html' }),
-    new ForkTsCheckerWebpackPlugin({ async: false }),
-    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
-  ],
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-    overlay: true,
-    progress: true,
-    proxy: {
-      '/api': {
-        target: process.env.BACKEND_HOST,
-        changeOrigin: true,
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      port: await getPort(8080),
+      hot: true,
+      historyApiFallback: true,
+      overlay: true,
+      progress: true,
+      proxy: {
+        '/api': {
+          target: process.env.BACKEND_HOST,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
+
+  if (isDevelopment) {
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    config.plugins.push(new ReactRefreshWebpackPlugin());
+  }
+
+  return config;
 };
 
-if (isDevelopment) {
-  config.plugins.push(new webpack.HotModuleReplacementPlugin());
-  config.plugins.push(new ReactRefreshWebpackPlugin());
-}
-
-module.exports = config;
+module.exports = getConfig();
